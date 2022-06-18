2cf57fd8017fe07c89f6b725647c0cb7fe6b3e5c
Neha <nehanemade466@gmail.com>
2022-06-18 23:50:06 +0530

    greedy_solution

---
+++ 59_N_meetigs_in_one_room.cpp
@@ -0,0 +1,74 @@
+/*
+There is one meeting room in a firm. There are N meetings in the form of (start[i], end[i]) where start[i] is start time of meeting i and end[i] is finish time of meeting i.
+What is the maximum number of meetings that can be accommodated in the meeting room when only one meeting can be held in the meeting room at a particular time?
+
+Note: Start time of one chosen meeting can't be equal to the end time of the other chosen meeting.
+
+
+Example 1:
+
+Input:
+N = 6
+start[] = {1,3,0,5,8,5}
+end[] =  {2,4,6,7,9,9}
+Output: 
+4
+Explanation:
+Maximum four meetings can be held with
+given start and end timings.
+The meetings are - (1, 2),(3, 4), (5,7) and (8,9)
+*/
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        // Your code here
+        vector<pair<int,int>>arr;
+        int i,count=0;
+        for(i=0;i<n;i++)
+        {
+            arr.push_back({end[i],start[i]});
+        }
+        sort(arr.begin(),arr.end());
+        int limit = -1;
+        for(i=0;i<n;i++)
+        {
+            if(arr[i].second>limit)
+            {
+                count++;
+                limit = arr[i].first;
+            }
+        }
+        return count;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
+
